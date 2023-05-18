5732f24bf4ccf2807f413ffd7961b07ec7fa34d5
Omar Ezzat <omar.ezzat@email.com>
2023-05-18 09:24:36 +0200

    use v1.3.5, add timeout

--- functions/hello-world/index.ts
+++ functions/hello-world/index.ts
@@ -6,11 +6,13 @@ interface reqPayload {
 
 console.log('hello-world started');
 
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 serve(async (req: Request) => {
   const { name } : reqPayload = await req.json();
   const data = {
     message: `Hello ${name} from Supabase Edge Functions!`,
   }
+  await wait(1000);
 
   return new Response(
     JSON.stringify(data),
--- functions/main/index.ts
+++ functions/main/index.ts
@@ -3,53 +3,7 @@ import * as jose from "https://deno.land/x/jose@v4.14.1/index.ts"
 
 console.log('main function started');
 
-const JWT_SECRET = Deno.env.get("JWT_SECRET");
-const VERIFY_JWT = Deno.env.get("VERIFY_JWT") === "true";
-
-function getAuthToken(req: Request) {
-  const authHeader = req.headers.get("authorization");
-  if (!authHeader) {
-    throw new Error("Missing authorization header");
-  }
-  const [bearer, token] = authHeader.split(" ");
-  if (bearer !== "Bearer") {
-    throw new Error(`Auth header is not 'Bearer {token}'`);
-  }
-  return token;
-}
-
-async function verifyJWT(jwt: string): Promise<boolean> {
-  const encoder = new TextEncoder();
-  const secretKey = encoder.encode(JWT_SECRET);
-  try {
-    await jose.jwtVerify(jwt, secretKey);
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-  return true;
-}
-
 serve(async (req: Request) => {
-  if (req.method !== "OPTIONS" && VERIFY_JWT) {
-    try {
-      const token = getAuthToken(req);
-      const isValidJWT = await verifyJWT(token);
-
-      if (!isValidJWT) {
-        return new Response(
-          JSON.stringify({ msg: "Invalid JWT"}),
-          { status: 401, headers: { "Content-Type": "application/json" } },
-        ) 
-      }
-    } catch (e) {
-      console.error(e);
-      return new Response(
-        JSON.stringify({ msg: e.toString()}),
-        { status: 401, headers: { "Content-Type": "application/json" } },
-      )
-    }
-  }
 
   const url = new URL(req.url);
   const {pathname} = url;
@@ -68,7 +22,7 @@ serve(async (req: Request) => {
   console.error(`serving the request with ${servicePath}`);
 
   const memoryLimitMb = 150;
-  const workerTimeoutMs = 1 * 60 * 1000;
+  const workerTimeoutMs = 1 * 1 * 100;
   const noModuleCache = false;
   const importMapPath = null;
   const envVarsObj = Deno.env.toObject();
@@ -85,6 +39,7 @@ serve(async (req: Request) => {
     });
     return await worker.fetch(req);
   } catch (e) {
+    console.error(e);
     const error = { msg: e.toString() }
     return new Response(
       JSON.stringify(error),
